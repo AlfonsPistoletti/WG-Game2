b103e96141fd2de911273169cb862b2240f26fab
Christoph <christophweinreich@googlemail.com>
2019-04-24 17:52:10 +0200

    Rakete pro Sekunde

--- WG-Game2/Assets/Scripts/RocketShooter.cs
+++ WG-Game2/Assets/Scripts/RocketShooter.cs
@@ -7,32 +7,47 @@ public class RocketShooter : MonoBehaviour
     Touch touch;
     public GameObject raketenPrefab;
     public float raketenSpeed = 2f;
+    public float touchCooldown = 1f;
+    float timer;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        Shoot();
+
+    }
+
+    void Shoot()
+    {
+        timer = timer + Time.deltaTime;
+
+        if (timer >= touchCooldown)
         {
-            touch = Input.GetTouch(0);
-            if(touch.phase == TouchPhase.Began) 
+
+            if (Input.touchCount > 0)
             {
-                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position) - this.transform.position;
-                touchPosition = touchPosition.normalized;
-                Debug.Log(touchPosition.ToString());
+                touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position) - this.transform.position;
+                    touchPosition = touchPosition.normalized;
+                    Debug.Log(touchPosition.ToString());
 
-                GameObject rakete = Instantiate(raketenPrefab, this.transform.position, raketenPrefab.transform.rotation) as GameObject;
+                    GameObject rakete = Instantiate(raketenPrefab, this.transform.position, raketenPrefab.transform.rotation) as GameObject;
 
-                 Rigidbody2D raketenRigid = rakete.GetComponent<Rigidbody2D>();
-                 raketenRigid.velocity = touchPosition * raketenSpeed;
+                    Rigidbody2D raketenRigid = rakete.GetComponent<Rigidbody2D>();
+                    raketenRigid.velocity = touchPosition * raketenSpeed;
+                    timer = 0f;
+                }
+
             }
-
         }
     }
 }
