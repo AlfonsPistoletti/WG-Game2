6966fc864807c98f1e4ce21a0ff99cd12a8c6ea7
Christoph <christophweinreich@googlemail.com>
2019-04-24 18:43:28 +0200

    Health1

---
+++ WG-Game2/Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+
+    public int playerHealth = 5;
+
+
+
+    public void LoseHealth()
+    {
+        playerHealth = playerHealth - 1;
+
+        if(playerHealth <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
