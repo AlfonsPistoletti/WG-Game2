8f9ad051c34e5eff998c0eecd960b880df7221fb
Christoph <christophweinreich@googlemail.com>
2019-04-24 19:17:15 +0200

    Samplescene

---
+++ WG-Game2/Assets/Scripts/alien.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alien : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float speed;
+    public GameObject explosion;
+    public GameObject health;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = Vector2.down * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Rakete")
+            || collision.gameObject.CompareTag("Player"))
+        {
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
+        }
+
+    }
+
+}
