24e4102a6d1fa9c9f08fe583b3fab1ef6cf89e4c
Yegboi <morlib@web.de>
2019-04-24 18:00:13 +0200

    spawner

---
+++ WG-Game2/Assets/Scripts/spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public float spawnIntervall = 1f;
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer = timer + Time.deltaTime;
+
+        if (timer >= spawnIntervall)
+        {
+            Vector3 randomPosition = new Vector3(this.transform.position.x,
+                                     this.transform.position.y + Random.Range(-3f, 3f), 0);
+            Instantiate(enemy, randomPosition, enemy.transform.rotation);
+            timer = 0f;
+        }
+
+    }
+}
