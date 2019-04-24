022e54126d410ba9f4064bc340af50347ba97e7b
Christoph <christophweinreich@googlemail.com>
2019-04-24 18:59:17 +0200

    Health

--- WG-Game2/Assets/Scripts/Health.cs
+++ WG-Game2/Assets/Scripts/Health.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
 
     public int playerHealth = 5;
+    public Text healthText;
 
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        healthText.text = playerHealth.ToString();
+    }
 
 
     public void LoseHealth()
