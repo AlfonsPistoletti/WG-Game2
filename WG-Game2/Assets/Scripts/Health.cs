7168b5ebe0c0755ee6a38386e441dd278cefab94
Christoph <christophweinreich@googlemail.com>
2019-04-26 13:06:56 +0200

    Screen Shake

--- WG-Game2/Assets/Scripts/Health.cs
+++ WG-Game2/Assets/Scripts/Health.cs
@@ -10,6 +10,13 @@ public class Health : MonoBehaviour
     public Text healthText;
     public GameObject healthExplosion;
 
+    ScreenShake shake;
+
+    private void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<ScreenShake>();
+    }
+
 
     private void Update()
     {
@@ -18,10 +25,12 @@ public class Health : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.CompareTag("Alien"))
         {
             //Debug.Log("Alien collided");
             Instantiate(healthExplosion, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+            shake.CamShake();
             Destroy(collision.gameObject);
             playerHealth = playerHealth - 1;
             RestartLevel();
---
+++ WG-Game2/Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    public Animator shakeAnim;
+
+
+    public void CamShake()
+    {
+        int rand = Random.Range(0, 2);
+        if (rand == 0)
+        {
+            shakeAnim.SetTrigger("Shake");
+            Handheld.Vibrate();
+            Debug.Log("Shake 1");
+        }
+
+        if (rand == 1)
+        {
+            shakeAnim.SetTrigger("Shake2");
+            Handheld.Vibrate();
+            Debug.Log("Shake 2");
+        }
+    }
+}
